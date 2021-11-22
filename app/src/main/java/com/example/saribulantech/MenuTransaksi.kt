8651844dcd392c9f2022b289ed2d158b90c5f7af
Dwisatriapatra <dwisatriapatra2000@gmail.com>
2021-11-22 16:28:32 +0700

    Update : Add "lihat data transaksi" function

--- app/src/main/java/com/example/saribulantech/MenuTransaksi.kt
+++ app/src/main/java/com/example/saribulantech/MenuTransaksi.kt
@@ -1,11 +1,22 @@
 package com.example.saribulantech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MenuTransaksi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transaksi)
+        setContentView(R.layout.activity_menu_transaksi)
+
+        val tambahDataTransaksi = findViewById<TextView>(R.id.TambahDataTransaksi)
+        val updateDataTransaksi = findViewById<TextView>(R.id.UpdateDataTransaksi)
+        val lihatDataTransaksi = findViewById<TextView>(R.id.LihatDataTransaksi)
+        val hapusDataTransaksi = findViewById<TextView>(R.id.HapusDataTransaksi)
+
+        lihatDataTransaksi.setOnClickListener {
+            startActivity(Intent(this, LihatDataTransaksi::class.java))
+        }
     }
 }
